36eb40c901421d4894d3f7872f1cc3f3a41f1ec1
Johnny Cruz <johnny@vermais.com>
2020-01-13 20:30:20 -0300

    Conclusão da 3° Etapa do Curso Javascript ES6

--- src/main.js
+++ src/main.js
@@ -208,6 +208,8 @@ const usuario = {
 
 console.log(usuario);
 
+///////////////////////////////////////////////////
+
 */
 
 // Webpack Server
@@ -228,11 +230,60 @@ console.log(sub(4,2));
 
 //alert('Hello World');
 
+///////////////////////////////////////////////////
+
+// Async/await
+
+//-- Async/await
+
+/*
+const minhaPromise = () => new Promise((resolve, reject) => {
+	setTimeout(() => { resolve('OK') }, 2000);
+});
+
+// minhaPromise()
+// 	.then(response => {
+// 		console.log(response);
+// 	})
+// 	.catch(err => {
+
+// 	});
+
+async function executaPromise() {
+	console.log(await minhaPromise());
+	console.log(await minhaPromise());	
+	console.log(await minhaPromise());
+}
+
+const executaPromise = async () => {
+	console.log(await minhaPromise());
+	console.log(await minhaPromise());	
+	console.log(await minhaPromise());
+};
+
+executaPromise();
+*/
+
+// -- Configurando Axios
+
+// import axios from 'axios';
+
+/*
+class Api {
+	static async getUserInfo(username) {
+		try {
+			const response = await axios.get(`https://api.github.com/users/${username}`)
+
+			console.log(response);
+		} catch (err) {
+			console.warn('Erro na API');
+		}
+	}
+}
+
+Api.getUserInfo('diego3g');
+*/
+
+
 //Desafios (Colar o desafio do "desafios.js" apartir daqui para testar)
-
-import ClasseUsuario, { idade as IdadeUsuario } from './functions';
-
-alert(IdadeUsuario);
-ClasseUsuario.info();
-
 
